8b5953455400ffde560de262ee78de60b11876dd
tzizel <timotheus.zimmermann@me.com>
2017-06-15 13:49:43 +0200

    Create ftapp.py

--- ftapp.py
+++ ftapp.py
@@ -1,4 +1,10 @@
-import ftplib 
+import ftplib
+import signal
+
+run = True
+
+while run:
+    pass # do stuff including other IO stuff
 
 path = 'dow/'
 filename = 'hello.py'
@@ -10,3 +16,10 @@ ftp.retrbinary("RETR " + filename ,open(filename, 'wb').write)
 ftp.quit()
 
 exec(open("./hello.py").read())
+
+def handler_stop_signals(signum, frame):
+    global run
+    run = False
+
+signal.signal(signal.SIGINT, handler_stop_signals)
+signal.signal(signal.SIGTERM, handler_stop_signals)
