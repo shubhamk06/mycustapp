3b6efebed8f079612ca81cb39a81cf0b1da1d2d0
tzizel <timotheus.zimmermann@me.com>
2017-06-19 13:18:10 +0200

    Update ftapp.py

--- ftapp.py
+++ ftapp.py
@@ -1,6 +1,13 @@
 import ftplib 
+import signal
+import time
 
-path = 'dow/'
+def handler_stop_signals(signum, frame):
+
+	signal.signal(signal.SIGINT, handler_stop_signals)
+	signal.signal(signal.SIGTERM, handler_stop_signals)
+	
+path = "dow/"
 filename = 'hello.py'
 
 ftp = ftplib.FTP("31.170.167.199") 
@@ -8,5 +15,4 @@ ftp.login("u454940376", "Timo1234!")
 ftp.cwd(path)
 ftp.retrbinary("RETR " + filename ,open(filename, 'wb').write)
 ftp.quit()
-
 exec(open("./hello.py").read())
