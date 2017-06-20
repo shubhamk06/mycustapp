a109efb1c7b13b80fde517c1db8341f9b11f821e
tzizel <timotheus.zimmermann@me.com>
2017-06-20 13:09:27 +0200

    Update ftapp.py

--- ftapp.py
+++ ftapp.py
@@ -3,6 +3,8 @@ import ftplib
 
 app = Flask(__name__)
 
+port = int(os.getenv("PORT"))
+
 @app.route('/')
 def hello_world():
     path = 'dow/'
@@ -16,4 +18,4 @@ def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-   app.run(host='0.0.0.0')
+   app.run(host='0.0.0.0', port=port)
