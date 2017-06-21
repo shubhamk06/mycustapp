c6ac3e7ea9b364e0636d29825e6f8628d8bd0ce4
tzizel <timotheus.zimmermann@me.com>
2017-06-21 14:21:36 +0200

    Update ftapp.py

--- ftapp.py
+++ ftapp.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import ftplib
 import os
+import boto3
 
 app = Flask(__name__)
 
@@ -16,6 +17,11 @@ def hello_world():
     ftp.retrbinary("RETR " + filename ,open(filename, 'wb').write)
     ftp.quit()
     exec(open("./hello.py").read())
+    conn = boto3.client('s3', region_name="eu-west-1", endpoint_url="https://ds31s3.swisscom.com", aws_access_key_id='5484335407854a4c9dc88e01206fc148/CF_P8_C2EF884E_EE05_4B55_A430_DAD9D9F9FE5E', aws_secret_access_key='xTTumTjwD1GCGZ+zyAzs9+pq+DE=',)
+    conn.create_bucket(Bucket="mytestbucket22")
+    filename = 'file.txt'
+    bucket_name = "mytestbucket22"
+    conn.upload_file(filename, bucket_name, filename)
     return 'Hello, World!'
 
 if __name__ == '__main__':
