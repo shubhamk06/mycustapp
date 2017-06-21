587d3db71868a9aab25ea48772e9cd591d6cc67f
tzizel <timotheus.zimmermann@me.com>
2017-06-21 14:41:25 +0200

    Update ftapp.py

--- ftapp.py
+++ ftapp.py
@@ -9,14 +9,8 @@ port = int(os.getenv("PORT"))
 
 @app.route('/')
 def hello_world():
-    path = 'dow/'
-    filename = 'hello.py'
-    ftp = ftplib.FTP("31.170.167.199") 
-    ftp.login("u454940376", "Timo1234!") 
-    ftp.cwd(path)
-    ftp.retrbinary("RETR " + filename ,open(filename, 'wb').write)
-    ftp.quit()
-    exec(open("./hello.py").read())
+    conn = boto3.client('s3', region_name="eu-west-1", endpoint_url="https://ds31s3.swisscom.com", aws_access_key_id='5484335407854a4c9dc88e01206fc148/CF_P8_C2EF884E_EE05_4B55_A430_DAD9D9F9FE5E', aws_secret_access_key='xTTumTjwD1GCGZ+zyAzs9+pq+DE=',)
+    conn.create_bucket(Bucket="mytestbucket22")
     return 'Hello, World!'
 
 if __name__ == '__main__':
